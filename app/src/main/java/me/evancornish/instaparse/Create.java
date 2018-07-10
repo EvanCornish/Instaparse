b14ef4a10841d5fc523949419f00b73141e984c8
Evan Cornish <ecornish@fb.com>
2018-07-10 11:06:34 -0700

    Inflated post menu

--- app/src/main/java/me/evancornish/instaparse/Create.java
+++ app/src/main/java/me/evancornish/instaparse/Create.java
@@ -1,7 +1,10 @@
 package me.evancornish.instaparse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Create extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Create extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_post, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miBack:
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.miPost:
+                //code for posting image
+                break;
+        }
+        return true;
     }
 }
